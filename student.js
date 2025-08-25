1a7bd325b2bde58a56e63594fe2a1ed1ff66cf2f
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-08-25 14:42:11 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,5 +1,19 @@
 // student.js - External JavaScript file for Lab assignments
-// Function to confirm navigation before jumping to external links
+
+// Lab 2 Variables (created at top of file, not in a function)
+var thename = "Brandon Chang";
+var my_message = "cs190 Javascript Lab 2";
+var id = 878;
+
+// Lab 2 Function - Display variable information in alert box
+function displayinfo871() {
+    var message = "Name: " + thename + "\n" + 
+                  "Message: " + my_message + "\n" + 
+                  "ID: " + id;
+    alert(message);
+}
+
+// Lab 1 Function - Confirm navigation before jumping to external links
 function confirmlink537(url) {
     var userConfirm = confirm("Do you want to navigate to " + url + "?");
     if (userConfirm) {
