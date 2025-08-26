33ee71e009442e46393c580963c783b6723f6804
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-08-25 17:54:09 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,21 +1,20 @@
-// Lab 1: confirm link function
+// ===== Lab 1 =====
 function confirmlink537(url) {
     if (confirm("Do you want to jump to " + url + "?")) {
         window.location.href = url;
     }
 }
 
-// Lab 2: declare variables (outside of any function)
+// ===== Lab 2 =====
 var thename = "Brandon Chang";
 var my_message = "cs190 Javascript Lab 2";
 var id = 878;
 
-// Lab 2: function to display info in alert
 function displayinfo871() {
     alert("Name: " + thename + "\nMessage: " + my_message + "\nID: " + id);
 }
 
-// Lab 3: fix syntax errors in displayspecial976
+// ===== Lab 3 =====
 var current = 1; // declare outside functions
 
 function displayspecial976() {
@@ -26,6 +25,7 @@ function displayspecial976() {
     } else if (current === 3) {
         alert("The current special is #3");
     }
+
     current = current + 1;
     if (current > 3) {
         current = 1; // wrap around
