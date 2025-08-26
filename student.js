cb741e1b0df28e61c4f4391f733f72589fb60ec4
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-08-25 18:41:20 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,3 +1,8 @@
+// Lab 1 function
+function confirmlink537(url) {
+    return confirm("Do you want to jump to " + url + "?");
+}
+
 // Lab 2 variables
 var thename = "Brandon Chang";
 var my_message = "cs190 Javascript Lab 2";
@@ -6,11 +11,6 @@ var id = 878;
 // Lab 2 function
 function displayinfo871() {
     alert("Name: " + thename + "\nMessage: " + my_message + "\nID: " + id);
-}
-
-// Lab 1 function
-function confirmlink537(url) {
-    return confirm("Do you want to jump to " + url + "?");
 }
 
 // Lab 3 variable
@@ -31,5 +31,36 @@ function displayspecial976() {
     current = current + 1;
     if (current > 3) {
         current = 1; // wrap around
+    }
+}
+
+// Lab 4 global variable
+var custtype = "direct"; // starts as direct
+
+// Lab 4 functions
+function changetype488() {
+    var newtype;
+    if (custtype == "direct") {
+        newtype = "advertising";
+    }
+    if (custtype == "advertising") {
+        newtype = "subscription";
+    }
+    if (custtype == "subscription") {
+        newtype = "direct";
+    }
+    custtype = newtype;
+    alert("Customer type changed to: " + custtype);
+}
+
+function displaytype493() {
+    if (custtype == "direct") {
+        alert("Buy Now! From this web page! My children need new shoes!");
+    }
+    if (custtype == "advertising") {
+        alert("Support our advertisers! Click on an ad, so I can make money!");
+    }
+    if (custtype == "subscription") {
+        alert("Renew your subscription today! My children need medicine!");
     }
 }
