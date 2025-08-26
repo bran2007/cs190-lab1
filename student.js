42d824e901c7b05a6f5639ca191f316c6221add3
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-08-25 18:42:36 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,22 +1,22 @@
-// Lab 1 function
+// Lab 1
 function confirmlink537(url) {
-    return confirm("Do you want to jump to " + url + "?");
+    if (confirm("Do you want to jump to " + url + "?")) {
+        window.location = url;
+    }
 }
 
-// Lab 2 variables
+// Lab 2
 var thename = "Brandon Chang";
 var my_message = "cs190 Javascript Lab 2";
 var id = 878;
 
-// Lab 2 function
 function displayinfo871() {
-    alert("Name: " + thename + "\nMessage: " + my_message + "\nID: " + id);
+    var msg = "Name: " + thename + "\nMessage: " + my_message + "\nID: " + id;
+    alert(msg);
 }
 
-// Lab 3 variable
-var current = 1; // selects which item is "on special"
-
-// Lab 3 function
+// Lab 3
+var current = 1;
 function displayspecial976() {
     if (current == 1) {
         alert("The current special is #1");
@@ -27,40 +27,45 @@ function displayspecial976() {
     if (current == 3) {
         alert("The current special is #3");
     }
-
-    current = current + 1;
+    current++;
     if (current > 3) {
-        current = 1; // wrap around
+        current = 1;
     }
 }
 
-// Lab 4 global variable
-var custtype = "direct"; // starts as direct
+// Lab 4
+var custtype = "direct";
 
-// Lab 4 functions
 function changetype488() {
     var newtype;
-    if (custtype == "direct") {
-        newtype = "advertising";
-    }
-    if (custtype == "advertising") {
-        newtype = "subscription";
-    }
-    if (custtype == "subscription") {
-        newtype = "direct";
-    }
+    if (custtype == "direct") newtype = "advertising";
+    else if (custtype == "advertising") newtype = "subscription";
+    else if (custtype == "subscription") newtype = "direct";
     custtype = newtype;
     alert("Customer type changed to: " + custtype);
 }
 
 function displaytype493() {
-    if (custtype == "direct") {
-        alert("Buy Now! From this web page! My children need new shoes!");
-    }
-    if (custtype == "advertising") {
-        alert("Support our advertisers! Click on an ad, so I can make money!");
-    }
-    if (custtype == "subscription") {
-        alert("Renew your subscription today! My children need medicine!");
-    }
+    if (custtype == "direct") alert("Buy Now! From this web page! My children need new shoes!");
+    else if (custtype == "advertising") alert("Support our advertisers! Click on an ad, so I can make money!");
+    else if (custtype == "subscription") alert("Renew your subscription today! My children need medicine!");
+}
+
+// Lab 5
+function getName998() {
+    var name = prompt("Enter your name:");
+    var message = "Your Name Is: " + name + " ";
+    if (name.length < 5) message += "You have a short name";
+    else if (name.length < 10) message += "You have a medium name";
+    else message += "You have a long name";
+    document.getElementById("name998").innerText = message;
+}
+
+function getAge412() {
+    var age = prompt("Enter your age:");
+    var message = "Your Age Is: " + age + " ";
+    if (age < 18) message += "You are a kid";
+    else if (age < 50) message += "You are an adult";
+    else message += "You are a senior citizen";
+    document.getElementById("age412").innerText = message;
 }
