87be116447477cdaf6fb9d5900546ebbd16e41d9
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-08-25 18:34:02 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,44 +1,35 @@
-// Lab 1
-function confirmlink537(url) {
-    if (confirm("Do you want to jump to " + url + "?")) {
-        window.location = url;
-    }
-}
-
-// Lab 2
+// Lab 2 variables
 var thename = "Brandon Chang";
 var my_message = "cs190 Javascript Lab 2";
 var id = 878;
 
+// Lab 2 function
 function displayinfo871() {
     alert("Name: " + thename + "\nMessage: " + my_message + "\nID: " + id);
 }
 
-// Lab 3
-var current = 1;
+// Lab 1 function
+function confirmlink537(url) {
+    return confirm("Do you want to jump to " + url + "?");
+}
 
+// Lab 3 variable
+var current = 1; // selects which item is "on special"
+
+// Lab 3 function
 function displayspecial976() {
-    if (current == 1) alert("The current special is #1");
-    if (current == 2) alert("The current special is #2");
-    if (current == 3) alert("The current special is #3");
-    current++;
-    if (current > 3) current = 1;
-}
+    if (current == 1) {
+        alert("The current special is #1");
+    }
+    if (current == 2) {
+        alert("The current special is #2");
+    }
+    if (current == 3) {
+        alert("The current special is #3");
+    }
 
-// Lab 4
-var custtype = "direct";
-
-function changetype488() {
-    var newtype;
-    if (custtype == "direct") newtype = "advertising";
-    else if (custtype == "advertising") newtype = "subscription";
-    else if (custtype == "subscription") newtype = "direct";
-    custtype = newtype;
-    alert("Customer type changed to " + custtype);
-}
-
-function displaytype493() {
-    if (custtype == "direct") alert("Buy Now! From this web page! My children need new shoes!");
-    else if (custtype == "advertising") alert("Support our advertisers! Click on an ad, so I can make money!");
-    else if (custtype == "subscription") alert("Renew your subscription today! My children need medicine!");
+    current = current + 1;
+    if (current > 3) {
+        current = 1; // wrap around
+    }
 }
