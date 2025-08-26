23579d857d952a50654c6393271433afc1ab5734
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-08-25 17:41:57 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,6 +1,15 @@
+// Lab 1: confirm link function
 function confirmlink537(url) {
-    var confirmJump = confirm("Do you want to go to this site?");
-    if (confirmJump) {
+    if (confirm("Do you want to jump to " + url + "?")) {
         window.location.href = url;
     }
+}
+
+// Lab 2: variables and display function
+var thename = "Brandon Chang";
+var my_message = "cs190 Javascript Lab 2";
+var id = 878;
+
+function displayinfo871() {
+    alert("Name: " + thename + "\nMessage: " + my_message + "\nID: " + id);
 }
