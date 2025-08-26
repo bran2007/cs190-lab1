0fce6ed1d8c0461e13638911c1c5af85d1bfd3bc
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-08-25 17:31:50 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,23 +1,22 @@
+// Lab 1 variables and functions
+function confirmlink537(url) {
+    if (confirm("Do you want to go to this link?")) {
+        window.location.href = url;
+    }
+}
+
+// Lab 2 variables
 var thename = "Brandon Chang";
 var my_message = "cs190 Javascript Lab 2";
 var id = 878;
 
 function displayinfo871() {
-    var message = "Name: " + thename + "\n" + 
-                  "Message: " + my_message + "\n" + 
-                  "ID: " + id;
+    var message = "Name: " + thename + "\nMessage: " + my_message + "\nID: " + id;
     alert(message);
 }
 
-function confirmlink537(url) {
-    var userConfirm = confirm("Do you want to navigate to " + url + "?");
-    if (userConfirm) {
-        window.open(url, '_blank');
-    }
-    return false;
-}
-
-var current = 1;
+// Lab 3 variables and function
+var current = 1;  // selects which item that is "on special"
 
 function displayspecial976() {
     if (current == 1) {
@@ -31,6 +30,6 @@ function displayspecial976() {
     }
     current = current + 1;
     if (current > 3) {
-        current = 1;
+        current = 1;  // wrap around
     }
 }
