629e242b9366622ac23932305c4419b777230754
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-08-25 17:39:27 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,33 +1,6 @@
-// Lab 1
 function confirmlink537(url) {
-    if (confirm("Do you want to jump to this site?")) {
-        window.location = url;
-    }
-}
-
-// Lab 2
-var thename = "Brandon Chang";
-var my_message = "cs190 Javascript Lab 2";
-var id = 878;
-
-function displayinfo871() {
-    alert("Name: " + thename + "\nMessage: " + my_message + "\nID: " + id);
-}
-
-var current = 1;  // selects which item is "on special"
-
-function displayspecial976() {
-    if (current == 1) {
-        alert("The current special is #1");
-    }
-    if (current == 2) {
-        alert("The current special is #2");
-    }
-    if (current == 3) {
-        alert("The current special is #3");
-    }
-    current = current + 1;
-    if (current > 3) {
-        current = 1;  // wrap around
+    var confirmJump = confirm("Do you want to go to this site?");
+    if (confirmJump) {
+        window.location.href = url;
     }
 }
