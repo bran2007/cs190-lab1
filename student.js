d27df764903c948d07da9d5a7616bed528988840
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-08-25 18:48:06 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,4 +1,4 @@
-// Lab 2 variables
+// Lab 1 & 2
 var thename = "Brandon Chang";
 var my_message = "cs190 Javascript Lab 2";
 var id = 878;
@@ -9,11 +9,18 @@ function displayinfo871() {
 
 // Lab 3
 var current = 1;
+
 function displayspecial976() {
-    if (current == 1) alert("The current special is #1");
-    if (current == 2) alert("The current special is #2");
-    if (current == 3) alert("The current special is #3");
-    current++;
+    if (current == 1) {
+        alert("The current special is #1");
+    }
+    if (current == 2) {
+        alert("The current special is #2");
+    }
+    if (current == 3) {
+        alert("The current special is #3");
+    }
+    current = current + 1;
     if (current > 3) current = 1;
 }
 
@@ -30,9 +37,13 @@ function changetype488() {
 }
 
 function displaytype493() {
-    if (custtype == "direct") alert("Buy Now! From this web page! My children need new shoes!");
-    else if (custtype == "advertising") alert("Support our advertisers! Click on an ad, so I can make money!");
-    else if (custtype == "subscription") alert("Renew your subscription today! My children need medicine!");
+    if (custtype == "direct") {
+        alert("Buy Now! From this web page! My children need new shoes!");
+    } else if (custtype == "advertising") {
+        alert("Support our advertisers! Click on an ad, so I can make money!");
+    } else if (custtype == "subscription") {
+        alert("Renew your subscription today! My children need medicine!");
+    }
 }
 
 // Lab 5
@@ -42,23 +53,24 @@ function getName998() {
     if (name.length < 5) message += "You have a short name";
     else if (name.length < 10) message += "You have a medium name";
     else message += "You have a long name";
-    document.getElementById("name998").innerText = message;
+    document.getElementById("name998").innerHTML = message;
 }
 
 function getAge412() {
-    var age = Number(prompt("Enter your age:"));
+    var age = prompt("Enter your age:");
     var message = "Your Age Is: " + age + " ";
     if (age < 18) message += "You are a kid";
     else if (age < 50) message += "You are an adult";
     else message += "You are a senior citizen";
-    document.getElementById("age412").innerText = message;
+    document.getElementById("age412").innerHTML = message;
 }
 
 // Lab 6
 function getBreaks876() {
     var num;
     do {
-        num = Number(prompt("Enter a number from 1 to 5:"));
+        num = prompt("Enter a number from 1 to 5:");
+        num = parseInt(num);
     } while (num < 1 || num > 5);
     var breaks = "";
     for (var i = 0; i < num; i++) {
@@ -70,11 +82,12 @@ function getBreaks876() {
 function getCards495() {
     var num;
     do {
-        num = Number(prompt("Enter a number from 2 to 7:"));
+        num = prompt("Enter a number from 2 to 7:");
+        num = parseInt(num);
     } while (num < 2 || num > 7);
     var cards = "";
     for (var i = 0; i < num; i++) {
         cards += "Card #" + i + " ";
     }
-    document.getElementById("cards495").innerText = cards;
+    document.getElementById("cards495").innerHTML = cards;
 }
